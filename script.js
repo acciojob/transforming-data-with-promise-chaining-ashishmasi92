9635a1cc83a2ae52122eabdd5a21b39bdd58db82
Ashish <ashishmasih92g@gmail.com>
2025-05-22 12:46:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
-//your JS code here. If required.
+let inputbox = document.querySelector("#ip")
+let btn = document.querySelector("#btn")
+let div = document.querySelector("#output")
+
+function update(e){
+	let input = inputbox.value;
+if(isNaN(input)){
+	div.innerText="please enter a valid number"
+	return
+}
+	
+	let p = new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+div.innerText = `Result:${input}`				
+
+			resolve(input)
+		},2000)
+	})
+	.then((res)=>{
+		return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				
+			let multi = res*2;
+			div.innerText= `Result:${multi}`
+			resolve(multi)
+			},2000)
+		})
+	})
+	.then((res)=>{
+		return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				
+			let multi = res-3;
+			div.innerText= `Result:${multi}`
+			resolve(multi)
+			},1000)
+		})
+	})
+	.then((res)=>{
+		return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				
+			let multi = res/2;
+			div.innerText= `Result:${multi}`
+			resolve(multi)
+			},1000)
+		})
+	})
+	.then((res)=>{
+		return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				
+			let multi = res+10;
+			div.innerText= `Final result:${multi}`
+			resolve(multi)
+			},1000)
+		})
+	})
+	.catch(err=>{
+		console.log(err.message)
+	})
+}
+
+
+btn.addEventListener("click",update)
